6be5525a097ab8c0554cf13ca84e184c22916861
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2021-03-26 20:56:08 -0300

    feat: add footer to layout

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <header-component />
     <router-view />
+    <footer-component />
   </div>
 </template>
 
 <script>
 import HeaderComponent from '@/core/components/HeaderComponent'
+import FooterComponent from '@/core/components/FooterComponent'
 
 export default {
   components: {
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   }
 }
 </script>
