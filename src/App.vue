a29dd1f05773767d24db6db867bc193463a5aff7
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2021-03-28 16:23:49 -0300

    feat: load previous address from localStorage

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,34 @@
 import HeaderComponent from '@/core/components/HeaderComponent'
 import FooterComponent from '@/core/components/FooterComponent'
 
+import { mapMutations } from 'vuex'
+
 export default {
   components: {
     HeaderComponent,
     FooterComponent
+  },
+
+  created() {
+    this.loadAddressStore()
+  },
+
+  methods: {
+    ...mapMutations('address', ['setAddress']),
+
+    loadAddressStore() {
+      const previousLocation = JSON.parse(
+        window.localStorage.getItem('location')
+      )
+      const previousFancyAddress = window.localStorage.getItem('fancyAddress')
+
+      if (previousLocation && previousFancyAddress) {
+        this.setAddress({
+          fancyAddress: previousFancyAddress,
+          location: previousLocation
+        })
+      }
+    }
   }
 }
 </script>
