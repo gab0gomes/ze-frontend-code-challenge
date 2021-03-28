6a5df8e07374201d2ba982d9e4685fbadc5c4ba5
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2021-03-28 16:26:28 -0300

    feat: create noAddressState component

---
+++ src/pages/ProductsPage/components/NoAddressStateComponent/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <p>
+      Nenhum endereço selecionado.
+      <router-link to="/">Clique aqui para selecionar um endereço.</router-link>
+    </p>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
