f541bb60ff4da644b71515353e801013a66eeee1
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2021-03-28 16:27:36 -0300

    feat: start productCard component

---
+++ src/pages/ProductsPage/components/ProductCardComponent/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h5>{{ product.title }}</h5>
+    <img :src="product.images[0].url" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  max-width: 48px;
+}
+</style>
