8e33d180344aed5519b39d26e2b9f125c2d021b9
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2021-03-28 16:29:17 -0300

    feat: simplify pocSearch query

---
+++ src/core/graphQL/queries/pocSearch.js
@@ -0,0 +1,7 @@
+export default `
+  query pocSearchMethod($now: DateTime!, $algorithm: String!, $lat: String!, $long: String!) {
+    pocSearch(now: $now, algorithm: $algorithm, lat: $lat, long: $long) {
+      id
+    }
+  }
+`
