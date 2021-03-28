61a16dc3fe010561039c3d3256efdb0f03c8deec
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2021-03-28 16:26:47 -0300

    feat: create noPocState component

---
+++ src/pages/ProductsPage/components/NoPocStateComponent/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <p>
+      Desculpe, mas não atendemos nessa área ainda... :( Talvez você queira
+      <router-link to="/">escolher outro endereço?</router-link>
+    </p>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
