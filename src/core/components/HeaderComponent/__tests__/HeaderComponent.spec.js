04364c1c61281ff96a47c02fa52c853e36103baa
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2021-03-28 16:37:17 -0300

    feat: update tests

--- src/core/components/HeaderComponent/__tests__/HeaderComponent.spec.js
+++ src/core/components/HeaderComponent/__tests__/HeaderComponent.spec.js
@@ -1,13 +1,24 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 
 import Header from '../index'
+import addresStore from '@/core/store/address'
 
 const localVue = createLocalVue()
+localVue.use(Vuex)
+const store = new Vuex.Store({
+  modules: {
+    address: {
+      ...addresStore
+    }
+  }
+})
 
 describe('HeaderComponent', () => {
   it('render correctly', async () => {
     const wrapper = shallowMount(Header, {
-      localVue
+      localVue,
+      store
     })
 
     expect(wrapper.element).toMatchSnapshot()
